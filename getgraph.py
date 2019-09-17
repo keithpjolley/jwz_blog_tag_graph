4bd50b5962803fbeb6826bdd3beb981afb62f620
Keith P. Jolley <keithpjolley@gmail.com>
2019-09-17 08:56:47 -0700

    added community colors to tables

--- getgraph.py
+++ getgraph.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: UTF8 -*-
 
-#import colorsys
 import community  # pip install python-louvain
 import json
 import networkx as nx
@@ -9,38 +8,51 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from sklearn.preprocessing import minmax_scale
+from scipy.stats import rankdata
 
 def makegraph(data):
-    # The worst place to do this. Done in <strike>d3</strike> css.
-    #color = lambda c,n : "#%02x%02x%02x" % tuple(int(255*c)
-    #        for c in colorsys.hsv_to_rgb(c/(n+1),1,1))
-    # make a plain edge list with labels, 'values' -> weight
+    ''' Given node/edge data run some maths to create and return
+        a graph with some stats embedded. Graph is d3 ready.
+    '''
+    # Make a plain edge list with labels, 'values' -> weight
     nodes = {node['id']: node['label'] for node in data['nodes']}
+    # This was for when jwz took out nodes but not the corresponding edges
     for edge in data['edges']:
         if edge['from'] not in nodes:
-            nodes[edge['from']] = 'TAG "{}" REDACTED FOR YOUR SAFETY'.format(edge['from'])
+            nodes[edge['from']] = 'TAG "{}" REDACTED'.format(edge['from'])
         if edge['to'] not in nodes:
-            nodes[edge['to']] = 'TAG {} REDACTED FOR YOUR SECURITY'.format(edge['to'])
+            nodes[edge['to']] = 'TAG {} REDACTED'.format(edge['to'])
     edges = [(nodes[edge['from']], nodes[edge['to']], {'weight': edge['value']})
                 for edge in data['edges']]
-    #G = nx.DiGraph()  # not a directed graph
     G = nx.Graph()
     G.add_edges_from(edges)
     # Find communities, pagerank, and eigenvector centrality.
     # for nodes, betweenness for edges.
-    #partition = community.best_partition(nx.Graph(G))
     partition = community.best_partition(G)
     nx.set_node_attributes(G, partition, 'community') 
-    pagerank = nx.pagerank_scipy(G, weight='weight')
-    nx.set_node_attributes(G, pagerank, 'pagerank') 
+    pr = nx.pagerank_scipy(G, weight='weight')
+    nx.set_node_attributes(G, pr, 'pagerank')
+    def rankit(d):
+        '''
+        Given dict with numerical values returns a dict with same keys
+        but with an ordinal ranking (lowest to highest).
+        Extra nonsense to get low to high rank and to get the rank as
+        an int instead of a numpy.int64 because json.dump barfs instead
+        of dumps when it gets an int64
+        '''
+        return dict(zip(d.keys(),
+            [int(j) for j in rankdata([-i for i in d.values()], method='max')]
+          ))
+    nx.set_node_attributes(G, rankit(pr), 'pagerank_rank')
     eic = nx.eigenvector_centrality_numpy(G)
     nx.set_node_attributes(G, eic, 'eic') 
+    nx.set_node_attributes(G, rankit(eic), 'eic_rank')
     bb = nx.edge_betweenness_centrality(G, weight='weight', normalized=False)
     nx.set_edge_attributes(G, bb, 'betweenness') 
-    # node color <- community  (do in javascript?)
-    #nx.set_node_attributes(G,
-    #    {k:color(v, max(partition.values())) for k,v in partition.items()},
-    #    'color') 
+    nx.set_edge_attributes(G, rankit(bb), 'betweenness_rank')
+    # Don't look at this.
+    nx.set_edge_attributes(G,
+        rankit({(s,t):list(w.values())[0] for s,t,w in edges}), 'weight_rank')
     return nx.node_link_data(G)
 
 def getdata(url):
@@ -76,4 +88,4 @@ if __name__ == "__main__":
     data = getdata(input)
     graph = makegraph(data)
     writegraph(graph, output)
-    # check with jq .< output
+    # check with jq . output
